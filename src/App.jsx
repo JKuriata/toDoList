34bd29617426bfb4908fe1d7f2c211ab5aa44e2a
JKuriata <JK271096@outlook.com>
2024-01-25 22:55:22 +0000

    Added the ability to add items to list along with some base CSS

--- src/App.jsx
+++ src/App.jsx
@@ -1,35 +1,34 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { useState } from 'react';
+import './App.css';
 
-function App() {
-  const [count, setCount] = useState(0)
+const App = () => {
+  const [item, setItem] = useState("");
+  const [items, setItems] = useState([]);
+
+  const handleChange = (event) => {
+    setItem(event.target.value);
+  };
+
+  const handleAdd = () => {
+    // Avoid adding empty items
+    if (item.trim() !== "") {
+      setItems((prevItems) => [...prevItems, item]);
+      setItem(""); // Clear the input after adding the item
+    }
+  };
 
   return (
     <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+      <h1>To-Do List</h1>
+      <div className="input">
+        <input value={item} onChange={handleChange}></input>
+        <button onClick={handleAdd}>Add</button>
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+      {items.map((todo, index) => (
+        <p key={index}>{todo}</p>
+      ))}
     </>
-  )
-}
+  );
+};
 
-export default App
+export default App;
